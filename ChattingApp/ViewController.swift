1e05eed059b07e5f3762bca0dffeb3c754d93555
jaeyong <maxwodyd98@gmail.com>
2020-10-11 07:39:11 +0900

    Ready for using Firebase service

--- ChattingApp/ViewController.swift
+++ ChattingApp/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        titleLabel.text = ""
+        var charIndex = 0
+        let titleText = "💬 ChattingApp"
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(charIndex), repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+                
+            }
+            charIndex += 1
+        }
+        
     }
 
 
